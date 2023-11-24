18d171bb5c572823a1063cb022c5cf91e0d49c3f
kalyan-nagakumar <nagakumar.work@gmai.com>
2023-11-23 19:41:54 -0600

    Initial Commit

---
+++ courseaPractice/ContentView.swift
@@ -0,0 +1,35 @@
+//
+//  ContentView.swift
+//  courseaPractice
+//
+//  Created by Kalyan Vajrala on 11/18/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    
+    @State var stepperValue: Int = 1
+    var body: some View {
+        VStack {
+            
+            
+            Stepper{
+              Text("Counter value")
+            }onIncrement: {
+                stepperValue += 1
+            }onDecrement: {
+                stepperValue = (stepperValue == 1) ? 1 : stepperValue - 1
+            }
+            
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ courseaPractice/courseaPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  courseaPracticeApp.swift
+//  courseaPractice
+//
+//  Created by Kalyan Vajrala on 11/18/23.
+//
+
+import SwiftUI
+
+@main
+struct courseaPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
